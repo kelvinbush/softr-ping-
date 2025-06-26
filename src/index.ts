363c4a2380c390e0a9b5d3dee380c1e2d2dbc546
Kelvin Bush <kelybush@gmail.com>
2025-06-26 13:39:09 +0300

    set up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Elysia } from "elysia";
+import {Elysia} from "elysia";
+import logger from "../utils/logger";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+logger.info(// @ts-ignore
+  ` Elysia is running at ${app.server?.protocol}://${app.server?.hostname}:${app.server?.port}`,);
